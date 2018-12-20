70552b24f7de97a4c105878f4c164b2fffcc3412
jagilley <37783831+jagilley@users.noreply.github.com>
2018-12-20 17:48:28 -0600

    added swift to a website (???)

---
+++ future.swift
@@ -0,0 +1,17 @@
+/*
+This is a Swift file with some thoughts in it!
+It's specifically a Swift file because people will be very confused to see a GitHub pages repo with a Swift file in it.
+
+Anyhoo, here are some cool background ideas:
+
+Space animation:
+https://codepen.io/jasperlachance/pen/QNMwBg
+
+Ambient bubbles:
+https://codepen.io/mimikos/pen/wKqyqY
+
+Weird gradient field thing:
+https://codepen.io/deathfang/pen/WxNVoq
+
+That is all.
+*/
